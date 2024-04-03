a5c2a808c13ab891445e307c4fb76c235716d2e0
winfredo <winfrednukpe2002@gmail.com>
2024-04-03 13:16:08 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Navbar from './components/navbar';
 function App() {
+
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar/>
+        <Routes>
+        <Route path="/"  /> 
+        <Route path="/cart"  />
+        </Routes>
+      </Router>
     </div>
   );
 }
